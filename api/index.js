e86eeaf5de7ab6b87d0695f26f8a5fee695a372b
ThienNotAc <thehitman199533@gmail.com>
2025-04-07 20:11:29 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,14 +1,4 @@
-// File: /api/index.js
-
-export default function handler(req, res) {
-  const frontendURL = "https://your-frontend-project-name.vercel.app"; // ⬅️ Replace with your actual frontend URL
-
-  const redirectMessage = `Redirecting you to ${frontendURL}\n`;
-  const jsonMessage = JSON.stringify({
-    success: true,
-    message: "Server is running"
-  });
-
-  res.setHeader("Content-Type", "text/plain");
-  res.status(200).send(redirectMessage + jsonMessage);
+{
+"success": true,
+"message": "Server is running"
 }
